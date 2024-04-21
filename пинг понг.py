a6c98096a6074e39a25e2541d120b8e7b5847e60
Варвара <166994266+iuyth@users.noreply.github.com>
2024-04-21 14:28:33 +0300

    Update пинг понг.py

---
+++ пинг понг.py
@@ -0,0 +1,49 @@
+from pygame import *
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_height, win_width))
+window.fill(back)
+
+racket1 = Player('...', 30, 200, 4, 50, 150)
+racket2 = Player('...', 530, 200, 4, 50, 150)
+ball = GameSprite('...', 200, 200, 4, 50, 50)
+
+game = True 
+finish = False
+clock = time.Clock()
+FPS = 60 
+#игровой цикл
+while game: 
+    for e in event.get():
+    if e.type==QUIT:
+        game = False 
+    display.update()
+    clock.tick(FPS)
+
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys=key.get_pressed()
+        if keys[K_w] and self.rect.y>5:
+            self.rect.y-=self.speed 
+         if keys[K_s] and self.rect.y<500:
+            self.rect.y+=self.speed 
+    def update_r(self):
+        keys=key.get_pressed()
+        if keys[K_UP] and self.rect.y>5:
+            self.rect.y-=self.speed 
+         if keys[K_DOWN] and self.rect.y<500:
+            self.rect.y+=self.speed
